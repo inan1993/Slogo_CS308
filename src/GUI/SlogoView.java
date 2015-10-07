e8070382f8894d5db9f909bda0e16375f5cfba6f
Abhishek Upadhyaya Ghimire <au15@duke.edu>
2015-10-07 14:03:38 -0400

    main methods setup

---
+++ src/GUI/SlogoView.java
@@ -0,0 +1,13 @@
+package src.GUI;
+
+import java.awt.Dimension;
+
+public class SlogoView {
+
+    public SlogoView(SlogoModel model, String string) {
+		// TODO Auto-generated constructor stub
+	}
+	public static final Dimension DEFAULT_SIZE = new Dimension(800, 600);
+    public static final String DEFAULT_RESOURCE_PACKAGE = "resources/";
+
+}
---
+++ src/Main.java
@@ -0,0 +1,28 @@
+package src;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import src.GUI.SlogoModel;
+import src.GUI.SlogoView;
+
+public class Main extends Application{
+    public static final String TITLE = "Slogo";
+    Scene scene;
+	
+	public  void start (Stage stage) throws Exception {
+        SlogoModel model = new SlogoModel();
+        SlogoView display = new SlogoView(model, "English");
+        stage.setTitle(TITLE);
+        stage.setScene(scene); 
+        stage.sizeToScene(); 
+        stage.show();
+    }
+	
+    public static void main (String[] args) {
+        launch(args);
+    }
+
+	
+
+}
