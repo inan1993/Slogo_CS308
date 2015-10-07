793eb9768df86c9194fc788b52d5fcd87f760492
rooper <logan.rooper@gmail.com>
2015-10-07 14:57:35 -0400

    Empty classes

---
+++ responses/Success.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package responses;
+
+/**
+ * @author loganrooper
+ *
+ */
+public class Success extends Response{
+
+	/* (non-Javadoc)
+	 * @see responses.Response#toString()
+	 */
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
