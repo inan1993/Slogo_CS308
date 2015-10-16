0b581668775176d7bc90b3a68a5ba654ffb8d679
rooper <logan.rooper@gmail.com>
2015-10-15 22:35:52 -0400

    children -> child

--- src/backend/parser/Parser.java
+++ src/backend/parser/Parser.java
@@ -145,7 +145,7 @@ public class Parser {
 		for(int i=0;i<numOfChildren;i++)
 		{
 			index++;
-			root.addChildren(factory.createNode(tokenList.get(index).getValue()));
+			root.addChild(factory.createNode(tokenList.get(index).getValue()));
 		}
 		return root;
 	}
