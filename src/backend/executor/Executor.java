2b9c89aec0c323e333bd24c29ecc16c96be6357d
rooper <logan.rooper@gmail.com>
2015-10-15 22:35:19 -0400

    Exec

--- src/backend/executor/Executor.java
+++ src/backend/executor/Executor.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import backend.node.Node;
 import responses.Response;
 import SharedObjects.*;
+import exceptions.*;
 
 /**
  * @author loganrooper
@@ -20,11 +21,17 @@ public class Executor {
 	 * @return
 	 */
 	public Response execute(Node root) {
-		
-		
 		if (root.hasChildren()) {
 			for (Node n : root.getChildren()) {
-				
+				switch (n.getClass().getName()) {
+				case "backend.node.Command":
+					//do command stuff
+					System.out.println("Command");
+				case "backend.node.ControlStructure":
+					throw new NotImplementedException();
+				case "backend.node.Variable":
+					throw new NotImplementedException();
+				}		
 			}
 		}
 		
---
+++ src/exceptions/NotImplementedException.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package exceptions;
+
+/**
+ * @author loganrooper
+ *
+ */
+public class NotImplementedException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+    public NotImplementedException(){}
+}
