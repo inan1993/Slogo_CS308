ac98cca0f39c4b4da63e39f4965566111b13b549
Abhishek Upadhyaya Ghimire <au15@duke.edu>
2015-10-15 21:54:57 -0400

    fixed turtlepane

--- src/GUI/TurtlePane/TurtlePane.java
+++ src/GUI/TurtlePane/TurtlePane.java
@@ -1,19 +1,26 @@
 package GUI.TurtlePane;
 
+import java.util.ResourceBundle;
+
 import GUI.SlogoModel;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
 public class TurtlePane extends Canvas{
     
-    private SlogoModel mySlogoModel;
+    //private SlogoModel mySlogoModel;
     private Turtle myTurtle;
     private GraphicsContext gc;
-    public TurtlePane (int width, int height, SlogoModel slogoModel) {
+    private static final String TURTLE_RESOURCE_PACKAGE = "GUI.TurtlePane/default";
+    protected static ResourceBundle myResource;
+
+    public TurtlePane (int width, int height) {
         super(width, height);
-        gc = this.getGraphicsContext2D();
-        this.mySlogoModel = slogoModel;
-        gc.setFill(mySlogoModel.getBackgroundColor());
+        myResource = ResourceBundle.getBundle(TURTLE_RESOURCE_PACKAGE);
+
+        gc = this.getGraphicsContext2D(); 
+        gc.setFill(Color.valueOf(myResource.getString("backgroundColors")));
         gc.fillRect(0,0,width,height);
         myTurtle = new Turtle();
         drawTurtle();
