969fd9118226197789ecbe9f326adb7c70bb1ce2
rooper <logan.rooper@gmail.com>
2015-10-15 22:51:37 -0400

    turtle

--- src/Main.java
+++ src/Main.java
@@ -16,14 +16,10 @@ public class Main extends Application{
         stage.setResizable(false);
         stage.sizeToScene(); 
         stage.show();
-
-        
-    }
+	}
 	
     public static void main (String[] args) {
         launch(args);
     }
-
-	
 
 }
