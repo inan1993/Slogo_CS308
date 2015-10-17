45b00e52c6a4f9a097bc5154e8c56e236fc36152
rooper <logan.rooper@gmail.com>
2015-10-16 23:41:51 -0400

    HT,ST

---
+++ src/backend/node/commands/HOME.java
@@ -0,0 +1,31 @@
+package backend.node.commands;
+
+import java.util.List;
+
+import SharedObjects.WorkSpaceController;
+import backend.node.Command;
+import backend.node.Constant;
+import backend.node.Node;
+import responses.Response;
+
+/**
+ * @author loganrooper
+ *
+ */
+public class HOME extends Command {
+	public HOME(String name, int children) {
+		super(name, children);
+	}
+
+	@Override
+	public Node run(WorkSpaceController sharedHandle, List<Node> ln) {
+		if (ln == null)
+			throw new RuntimeException("Missing parameter.");
+		
+		Response s = sharedHandle.home();
+		double distanceMoved = Double.parseDouble(s.toString());
+		
+		// return the delta
+		return new Constant("Moved").setValue(distanceMoved);
+	}
+}
