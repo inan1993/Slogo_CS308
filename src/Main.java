5e3c8c6a06fd7a20693467e689c8123455391b50
Connor Usry <cgu1001@gmail.com>
2015-10-17 18:28:19 -0400

    Moves HandleObservers into its own class so it can be publically accessible

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ import GUI.SlogoView;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sharedobjects.HandleObservers;
 import sharedobjects.ManipulateController;
 import sharedobjects.Workspace;
 
@@ -18,12 +19,6 @@ public class Main extends Application{
     public static final String TITLE = "Slogo";
     //private Map<Observable, Observer> observingMap;
     private static SlogoView frontEnd;
-   
-    //map of Observable (String name) to their Observers (List<String name>)
-    private static final Map<String, List<String>> observableMap = new HashMap<String,List<String>>() {{
-        put("Turtle", new LinkedList<String>(Arrays.asList("Observer1","Observer2")));
-    }};
-    
     Scene scene;
 	
 	public  void start (Stage stage) throws Exception {
@@ -35,41 +30,17 @@ public class Main extends Application{
     }
 	
     public static void main (String[] args) {
-        Workspace currWorkspace = new Workspace();
-        	//workspace must create an observables list
-        frontEnd = new SlogoView("English");
-        	//frontend must create an observers list
+    	/*1*/Workspace currWorkspace = new Workspace(); //TODO: workspace must create an observables list
+    	/*2*/ManipulateController manipulateController = new ManipulateController(currWorkspace);
+    	
+    	/*3*/ //Executor executor = new Executor(manipulateController);
+    	/*4*/ //Parser parser = new Parser(executor);
+    	
+    	/*5*/ frontEnd = new SlogoView("SEND PARSER HERE"); //TODO: frontEnd must create an observers list
         
-        //handleObservers(currWorspace.getObservables(), frontEnd.getObservers());
-        
-        ManipulateController manipulateController = new ManipulateController(currWorkspace);
-        //Executor executor = new Executor(manipulateController);
-        //Parser parser = new Parser(executor);
+    	HandleObservers handleObservers = new HandleObservers();   
+    	/*6*/ //HandleObservers.handleObservers(currWorspace.getObservables(), frontEnd.getObservers());
         launch(args);
         
     }
-   
-
-    //need to have the observers and obvervables linked by the workspace & frontend
-	private void handleObservers(List<Observable> observablesOList, List<Observer> observersOList){
-    	for(Observable observableO: observablesOList){
-    		String observableName = observableO.getClass().getName();
-    		List<String> observersSList = observableMap.get(observableName);
-    		if(observersSList != null){
-    			for(Observer observerO: observersOList){
-    				String observerName = observerO.getClass().getName();	
-    				if(observersSList.contains(observerName)){
-    					linkObserverableObserver(observableO, observerO);
-    				}
-    			}
-    		}
-    	}
-    }
-
-	//TODO: Add observer link to observable 
-	private void linkObserverableObserver(Observable observableO, Observer observerO) {
-		//Perform Observer/Observable Linking
-		observableO.addObserver(observerO);
-		
-	}
 }
