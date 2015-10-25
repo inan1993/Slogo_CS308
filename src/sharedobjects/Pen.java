caa721aad12a568d1cea3357d5731c89bb937e0d
Connor Usry <cgu1001@gmail.com>
2015-10-24 23:38:11 -0400

    Adds Pen Object and default pen states to properties file

--- src/sharedobjects/Pen.java
+++ src/sharedobjects/Pen.java
@@ -1,31 +1,50 @@
 package sharedobjects;
 
+import java.util.ResourceBundle;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 
 public class Pen {
-	
-	private Color color;
-	private double size;
+
+	private Paint color;
+	private double thickness;
+	private String state;
+
+	private static final String DEFAULT_RESOURCE_PACKAGE = "resources.languages/";
+    private static final String DEFAULT_RESOURCE_VIEW = "GUI.view";
+    private static ResourceBundle myResource;
+    private static final String DEFAULT_LANGUAGE = "English";
 	
 	public Pen() {
-
+		ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + DEFAULT_LANGUAGE);
+        myResource = ResourceBundle.getBundle(DEFAULT_RESOURCE_VIEW);
+		color =  Paint.valueOf(myResource.getString("defaultPenColor"));
+		thickness = Integer.parseInt(myResource.getString("defaultPenThickness"));
+		state = myResource.getString("defaultPenState");
 	}
 	
 	public void setColor(Color c){
 		color = c;
 	}
 	
-	public Color getColor(){
+	public Paint getColor(){
 		return color;
 	}
 	
-	public void setSize(double s){
-		size = s;
+	public void setThickness(double t){
+		thickness = t;
 	}
 	
-	public double getSize(){
-		return size;
+	public double getThickness(){
+		return thickness;
 	}
 	
+	public String getState() {
+		return state;
+	}
+	
+	public void setState(String state) {
+		this.state = state;
+	}
 
 }
