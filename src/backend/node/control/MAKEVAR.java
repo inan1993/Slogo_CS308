d0489e5516181019fc5e203dced54819233f2de1
rooper <logan.rooper@gmail.com>
2015-10-26 00:56:10 -0400

    make works

--- src/backend/node/control/MAKEVAR.java
+++ src/backend/node/control/MAKEVAR.java
@@ -11,16 +11,10 @@ import responses.Success;
 import sharedobjects.ManipulateController;
 
 public class MAKEVAR extends ControlStructure {
-
-//	@Override
-//	public Response run(ManipulateController mc) {
-//		Node var = getChild(0);
-//		Node result = new Constant().setValue(Double.parseDouble(executor.execute(nl.get(1)).toString()));
-//		mc.setVariable(var.getName(), result);
-//		return new Success(getChild(1).getDoubleValue());
-//	}
 	@Override
 	public Response run(ManipulateController mc) {
-		return new Error("Unimplemented");
+		String variableName = get(0).getName();
+		Double result = getAndRun(1, mc).getDoubleValue();
+		return mc.setVariable(variableName, new Constant(result));
 	}
 }
