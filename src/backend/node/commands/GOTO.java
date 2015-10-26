e5c0817c24853795f3f3dd678bd295a69d903d88
Connor Usry <cgu1001@gmail.com>
2015-10-25 21:22:16 -0400

    Adds GOTO lambda integration, but FAILS due to parser saying it is an undefined command

--- src/backend/node/commands/GOTO.java
+++ src/backend/node/commands/GOTO.java
@@ -2,9 +2,12 @@ package backend.node.commands;
 
 
 import backend.node.types.TwoArgumentNode;
+import datatransferobjects.TurtleTransferObject;
 import responses.Response;
 import responses.Success;
+import sharedobjects.LambdaInterface;
 import sharedobjects.ManipulateController;
+import sharedobjects.Turtle;
 
 /**
  * @author loganrooper
@@ -14,20 +17,16 @@ public class GOTO extends TwoArgumentNode {
 
 	@Override
 	public Response run(ManipulateController sharedHandle) {
-		// get headings
-		double prevHeading = sharedHandle.getHeading();
-
 		// get xy
 		int x = getAndRun(0, sharedHandle).getIntegerValue();
 		int y = getAndRun(1, sharedHandle).getIntegerValue();
-
-		// turn
-		sharedHandle.towards(x, y);
-
-		// new heading
-		double newHeading = getAndRun(0, sharedHandle).getDoubleValue();
-
-		// return the delta
-		return new Success(prevHeading - newHeading);
+				
+		LambdaInterface l = (Turtle t) -> {
+			TurtleTransferObject dto = new TurtleTransferObject(false, t.getID(), false, t.isPenDown(), t.getPosition(), new int[]{0,0});
+			t.setPosition(new int[]{x,y});
+			t.notifyObservers(dto);
+		};
+		sharedHandle.execute(l);
+		return new Success(0);
 	}
 }
