cb069127376aa4b77eb94dd1886dbe507fdff090
rooper <logan.rooper@gmail.com>
2015-10-26 00:53:15 -0400

    for works

--- src/backend/node/commands/TELL.java
+++ src/backend/node/commands/TELL.java
@@ -17,7 +17,6 @@ public class TELL extends OneArgumentNode {
 	public Response run(ManipulateController mc) {
 		// Call setTurtleIDS(array of ints) -> this should create turtles as well
 		// If only one argument then I'll create a range 0->x
-		
 		return new Success(0);
 	}
 }
