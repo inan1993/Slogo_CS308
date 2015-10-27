3ace6efee786ea2114a3a2cdaf488bbf4e5b9d02
rooper <logan.rooper@gmail.com>
2015-10-26 22:15:55 -0400

    Variables and function display mostly working

--- src/observers/FunctionVariableObserver.java
+++ src/observers/FunctionVariableObserver.java
@@ -1,11 +1,13 @@
 package observers;
 
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
 import GUI.viewbox.FunctionListBox;
 import GUI.viewbox.VariableListBox;
-import datatransferobjects.ParsedCommandsTransferObject;
+import sharedobjects.Functions;
+import sharedobjects.Variables;
 
 public class FunctionVariableObserver implements Observer{
 
@@ -19,14 +21,16 @@ public class FunctionVariableObserver implements Observer{
 
     @Override
     public void update (Observable o, Object arg) {
-        ParsedCommandsTransferObject functionVariableDTO = (ParsedCommandsTransferObject) arg;
-        System.out.println("variable observer");
-
-        if(functionVariableDTO.isFunction()){
-            myFunctionList.setMessage(functionVariableDTO.getUserInput()); 
+        if(arg.equals("Function")){
+        	List<String> s = ((Functions) o).getAllFunctionsAsString();
+        	for (String a : s)
+        		myFunctionList.setMessage(a); 
         }
-        else{
-            myVariableList.setMessage(functionVariableDTO.getUserInput()); 
+        else {
+        	//Variable
+        	List<String> s = ((Variables) o).getAllVariables();
+        	for (String a : s)
+        		myVariableList.setMessage(a); 
 
         }
     }
--- src/sharedobjects/Variables.java
+++ src/sharedobjects/Variables.java
@@ -3,9 +3,12 @@
  */
 package sharedobjects;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Observable;
+
 import backend.node.Node;
 import backend.node.types.Constant;
 
@@ -13,22 +16,27 @@ import backend.node.types.Constant;
  * @author loganrooper
  *
  */
-public class Variables extends Observable{
+public class Variables extends Observable {
 	Map<String, Node> variableMap;
-	
+
 	public Variables() {
 		variableMap = new HashMap<String, Node>();
-		this.setChanged();
+		update();
 	}
-	
+
 	public void setVariable(String variableName, Node var) {
 		addVariable(variableName, var);
-		this.setChanged();
+		update();
 	}
-	
+
+	private void update() {
+		setChanged();
+		notifyObservers("Variable");
+	}
+
 	public void addVariable(String v, Node n) {
 		variableMap.put(v, n);
-		this.setChanged();
+		update();
 	}
 
 	public Node getVariable(String variableName) {
@@ -40,12 +48,24 @@ public class Variables extends Observable{
 	}
 
 	public void incrementVariableByValue(String variableName, int value) {
-		double n = variableMap.get(variableName).getDoubleValue()+value;
+		double n = variableMap.get(variableName).getDoubleValue() + value;
 		variableMap.put(variableName, new Constant(n));
-		this.setChanged();
+		update();
 	}
 
 	public void decrementVariable(String variableName) {
 		incrementVariableByValue(variableName, -1);
+	}
+
+	public List<String> getAllVariables() {
+		ArrayList<String> n = new ArrayList<String>();
+		for (Map.Entry<String, Node> entry : variableMap.entrySet()) {
+			String s = "";
+			s += entry.getKey();
+			s += " : ";
+			s += entry.getValue().getDoubleValue();
+			n.add(s);
+		}
+		return n;
 	}
 }
